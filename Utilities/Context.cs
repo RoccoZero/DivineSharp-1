cbf67112a836e5df88dfe9ab9a94cfbe9ea1c4a9
ROCKFAL1 <73743539+ROCKFAL1@users.noreply.github.com>
2021-02-24 12:33:45 +0300

    +Utilities

---
+++ Utilities/Context.cs
@@ -0,0 +1,27 @@
+﻿using Divine.Menu;
+using Divine.Menu.Items;
+using Utilities.Modules;
+
+namespace Utilities
+{
+    internal sealed class Context
+    {
+        public readonly RootMenu rootMenu;
+
+        public readonly Antislark antislark;
+        public readonly Creepblocker creepblock;
+
+        public Context()
+        {
+            rootMenu = MenuManager.CreateRootMenu("Utilities");
+            antislark = new Antislark(this);
+            creepblock = new Creepblocker(this);
+        }
+        public void Dispose()
+        {
+            Antislark.Dispose();
+            Creepblocker.Dispose();
+        }
+
+    }
+}
